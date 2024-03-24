7ed5ae1fc4ae34ff79e670cfb8f9ce6905c74797
ruslan0003 <ruslan0003@yandex.ru>
2024-03-25 01:24:18 +0300

    refactor: Вернуть фильтры и заголовок в virtualize

--- src/components/catalog/catalogWindow.js
+++ src/components/catalog/catalogWindow.js
@@ -5,14 +5,31 @@ import Card from '../card/card';
 
 const children = [];
 for (let i = 0; i < 1000; i++) {
-  children.push(<Card key={i} number={i+1}/>);
+    children.push(<Card key={i} number={i + 1} />);
 }
 
 function CatalogWindow() {
-    return(
-<VirtualizedGrid itemCount={children.length} rowHeight={750} cellWidth={450} gridHeight={800}>
-			{(index) => <div>{children[index]}</div>}
-		</VirtualizedGrid>
+    return (
+        <section className="catalog">
+            <div className="catalog__upper-row">
+                <h3 className="catalog__title">Предстоящие фестивали</h3>
+                <div className="catalog__filters">
+                    <select className="catalog__filter">
+                        <option>Лето</option>
+                        <option>Зима</option>
+                    </select>
+                    <select className="catalog__filter">
+                        <option>Сначала новые</option>
+                        <option>По популярности</option>
+                    </select>
+                </div>
+            </div>
+            <VirtualizedGrid itemCount={children.length} rowHeight={750} cellWidth={425} gridHeight={1200}>
+                {(index) => <div>{children[index]}</div>}
+            </VirtualizedGrid>
+            <button className="button button_purple catalog__button-more">Смотреть все фестивали&nbsp;&rarr;</button>
+
+        </section>
     )
 }
 
